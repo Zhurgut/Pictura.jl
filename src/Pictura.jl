11bf2615537c525ecf233a798819ab5e825d3e2d
zhurgut <damian.ca@bluewin.ch>
2026-02-12 16:39:04 +0100

    fix and test julia bindings

--- src/Pictura.jl
+++ src/Pictura.jl
@@ -1,5 +1,12 @@
 module Pictura
 
-greet() = print("Hello World!")
+
+include("picturalib.jl")
+
+PicturaLib.init(500, 500)
+PicturaLib.draw_background(PicturaLib.get_canvas(), Float32(17 / 255), Float32(183 / 255), Float32(209 / 255), Float32(1.0))
+PicturaLib.present()
+PicturaLib.delay(1000000000)
+PicturaLib.quit()
 
 end # module Pictura
