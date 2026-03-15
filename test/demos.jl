53be0f8dcb031e99f4545e5e19eb5f15823f0a26
zhurgut <damian.ca@bluewin.ch>
2026-03-15 22:01:33 +0100

    add readme demos

--- test/demos.jl
+++ test/demos.jl
@@ -3,6 +3,93 @@ using ProceduralNoise
 using PicturaShapes
 
 
+
+@pictura begin
+    setup(size(600, 400))
+    nostroke()
+    background(0)
+
+    @drawloop begin
+        if mouse().l
+            fillcolor(rand(), rand(), rand())
+            circle(mouse().x, mouse().y, 10)
+        end
+    end
+end
+
+@pictura begin
+    setup(size(600, 400))
+    framerate(50)
+
+    background(255)
+    strokecolor(0)
+    strokewidth(1.2)
+
+    bg_color = color(255, 255, 255, 0.02)
+    alpha = 0.3
+    ϕ = 0.0
+    speed = 0.04
+    darkmode = false
+
+    @mousepressed begin
+        if BUTTON == RIGHT
+            if darkmode
+                background(255)
+                bg_color = color(255, 255, 255, 0.02)
+                strokecolor(0)
+                darkmode=false
+            else
+                background(0)
+                bg_color = color(0, 0, 0, 0.02)
+                strokecolor(255)
+                darkmode=true
+            end
+        elseif BUTTON == LEFT
+            speed += 1.0
+        end
+    end
+    
+    @drawloop begin
+        background(bg_color)
+        translate(width()/2, height()/2)
+        rotate(ϕ)
+
+        D = height() / 4
+        H = D / sqrt(3)
+        radius = height() / 4.5
+
+        fillcolor(57, 151, 70, alpha)
+        circle(0, -H, radius)
+
+        fillcolor(146, 89, 163, alpha)
+        circle(+0.5D, sqrt(3/4)*D-H, radius)
+
+        fillcolor(202, 60, 50, alpha)
+        circle(-0.5D, sqrt(3/4)*D-H, radius)
+
+        speed = max(speed * 0.98, 0.04)
+        ϕ += speed
+    end
+end
+
+
+
+function percolation(p)
+    @pictura begin
+
+    setup(size(800, 600))
+    img = Pictura.create_image(width() ÷ 2, height() ÷ 2)
+    for r=1:height(img), c=1:width(img)
+        pixels(img)[r, c] = rand() > p ? color(1.0) : color(0.0)
+    end
+    updatepixels(img)
+    @drawloop begin
+        image(img, nearest_sampling=true)
+    end
+
+    end
+end
+
 @pictura begin
     setup(size(800, 600))
     framerate(50)
